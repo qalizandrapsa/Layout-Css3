a8c85968bbd198c8d0c4db8231abc710e13c6855
QALizandrapsa <86857524+qalizandrapsa@users.noreply.github.com>
2021-07-07 01:08:56 -0300

    Add files via upload

---
+++ css3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Css3 </title>
+        <link rel="stylesheet" href="estilo/estilo.css" type="text/css">
+    </head>
+    <body>
+        <div id="barra">
+            <h1> Barra Superior </h1>
+        </div>
+        <div id="conteudo">
+            <h1> Conteudo </h1>
+        </div>
+        <div id="rodape">
+            <address> 2019 - Todos os direitos reservados </address>
+        </div>
+    </body>
+</html>
